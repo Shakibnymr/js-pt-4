bc61e6fe9560b52d167864b86f4fcac0329b41fe
shakibnymr <shakibnymr11@gmail.com>
2023-08-03 03:34:48 +0600

    completed

---
+++ 5.js
@@ -0,0 +1,48 @@
+// when the signal is red
+var signal = "red"; 
+
+if (signal === "green") {
+    console.log("Cross the road");
+} else if (signal === "yellow") {
+    console.log("Stop");
+} else if (signal === "red") {
+    console.log("Do not cross the road, wait for the green signal");
+} else {
+    console.log(" there is no signal color");
+}
+// when the signal is green
+var signal = "green"; 
+
+if (signal === "green") {
+    console.log("Cross the road");
+} else if (signal === "yellow") {
+    console.log("Stop");
+} else if (signal === "red") {
+    console.log("Do not cross the road, wait for the green signal");
+} else {
+    console.log(" there is no signal color");
+}
+// when the signal is yellow
+var signal = "yellow"; 
+
+if (signal === "green") {
+    console.log("Cross the road");
+} else if (signal === "yellow") {
+    console.log("Stop");
+} else if (signal === "red") {
+    console.log("Do not cross the road, wait for the green signal");
+} else {
+    console.log(" there is no signal color");
+}
+// when there is no signal
+var signal = "no signal"; 
+
+if (signal === "green") {
+    console.log("Cross the road");
+} else if (signal === "yellow") {
+    console.log("Stop");
+} else if (signal === "red") {
+    console.log("Do not cross the road, wait for the green signal");
+} else {
+    console.log(" there is no signal ");
+}
